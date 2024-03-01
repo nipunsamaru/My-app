49cc07fc5fbafe8b70167e2a559ad15c15c1048f
nipunsamaru <nipunsamaru@gmail.com>
2024-03-01 12:19:56 +0530

    changes

---
+++ my.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const my = () => {
+  return (
+    <View>
+      <Text>my</Text>
+    </View>
+  );
+};
+
+export default my;
+
+const styles = StyleSheet.create({});
